4322cf16093dd8df3efe2e51d6cbefced64792ff
Manikandan <manikandansundar18@gmail.com>
2025-03-21 20:52:51 +0530

    commit 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Add interactivity for progress bars
+
 document.addEventListener("DOMContentLoaded", function () {
   const progressBars = document.querySelectorAll(".progress");
   progressBars.forEach((bar) => {
     const width = bar.style.width;
-    bar.style.width = "0"; // Reset width for animation
+    bar.style.width = "0"; 
     setTimeout
